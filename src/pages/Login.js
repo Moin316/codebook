516941129e9b3785bd0dddfe8a215f72429aa4ec
Shaik Moin <moinshaik1603@gmail.com>
2025-02-11 21:36:08 +0530

    Changes Made

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,8 +25,8 @@ export const Login = () => {
   }
   
   async function handleLoginGuest(){
-    email.current.value = process.env.REACT_APP_GUEST_LOGIN;
-    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
+    email.current.value = "tryemail@gmail.com";
+    password.current.value = "password";
     try{
       const authDetail = {
         email: email.current.value,
